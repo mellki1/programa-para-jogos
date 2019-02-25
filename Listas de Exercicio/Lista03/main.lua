6005ca6ce314aa4ac207b6832f85e201ed388bbc
mellki1 <36212451+mellki1@users.noreply.github.com>
2019-02-25 17:21:35 -0300

    Commit Melqui 25-02
    Quase terminando, falta por a dificuldade

--- Listas de Exercicio/Lista03/main.lua
+++ Listas de Exercicio/Lista03/main.lua
@@ -48,6 +48,7 @@ local function gerarDados()
 end
 
 gerarDados()
+
 --variaveis de erros e acertos
 local acerto = 0
 local erro = 0
@@ -60,28 +61,35 @@ local h = display.contentHeight
 local quadrados = {}
 local resp = {}
 
-local placar = display.newText("Certas "..acerto.." x "..erro.." Erradas", w *.5, h * .1, native.systemFont, 32)
-circle1 = display.newCircle( w *.22, h * .3, 60)
---Sinal de adicao/mulltiplicacao
-local sinal = display.newText("+", w *.5, h * .3, native.systemFont, 60)
-circle2 = display.newCircle( w *.76, h * .3, 60)
 
-local number1 = display.newText(numbersF[1], w *.22, h * .3, native.systemFont, 60)
-number1:setFillColor(0, 0, 0)
+local backgorund = display.newImage("back.jpg", w *.22, h * .30) --backgorund
+backgorund:setFillColor(0.3)
+local placar = display.newText("Certas "..acerto.." x "..erro.." Erradas", w *.5, h * .1, native.systemFont, 32)--Placar do game
 
-local number2 = display.newText(numbersF[2], w *.76, h * .3, native.systemFont, 60)
-number2:setFillColor(0, 0, 0)
 
---gerando quadrados como respostas
-for i=1, 4 do
-    quadrados[i] = display.newRect(w * (i*2)/10, h *.9, 60, 50)
+local function tela()
+    circle1 = display.newCircle( w *.22, h * .3, 60)
+    --Sinal de adicao/mulltiplicacao
+    local sinal = display.newText("+", w *.5, h * .3, native.systemFont, 60)
+    circle2 = display.newCircle( w *.76, h * .3, 60)
+
+    local number1 = display.newText(numbersF[1], w *.22, h * .3, native.systemFont, 60)
+    number1:setFillColor(0, 0, 0)
+
+    local number2 = display.newText(numbersF[2], w *.76, h * .3, native.systemFont, 60)
+    number2:setFillColor(0, 0, 0)
+
+    --gerando quadrados como respostas
+    for i=1, 4 do
+        quadrados[i] = display.newRect(w * (i*2)/10, h *.9, 60, 50)
+    end
+
+    for i=1, 4 do
+        resp[i] = display.newText(respostaF[i], w * (i*2)/10, h *.9, native.systemFont, 32)
+        resp[i]:setFillColor(0, 0, 0)
+    end
 end
-
-for i=1, 4 do
-    resp[i] = display.newText(respostaF[i], w * (i*2)/10, h *.9, native.systemFont, 32)
-    resp[i]:setFillColor(0, 0, 0)
-end
-
+tela()
 --Funcionalidades
 --Variaveis necessarias para as funcionalidades
 local o = 0
@@ -94,7 +102,8 @@ end
 
 --evento
 local function clica(event)
-    o = tonumber(event.target.myName)
+
+    o = tonumber(event.target.myName) --convertendo os nomes dos quadrados para inteiro
     if respostaF[o] == certaF then
         acerto = acerto + 1
         print("acertos: "..acerto)
@@ -102,9 +111,15 @@ local function clica(event)
         erro = erro + 1
         print("erradas: "..erro)
     end
-    gerarDados()
+    if(erro >= 5) then
+        backgorund = display.newImage("gameOver.jpg", w *.22, h * .30)
+    else
+        gerarDados()
+        placar.text = ("Certas "..acerto.." x "..erro.." Erradas")
+        tela()
+    end
 end
-print("CERTA: "..certaF)
+
 for i=1, #quadrados do
     quadrados[i]:addEventListener("tap", clica)
 end
