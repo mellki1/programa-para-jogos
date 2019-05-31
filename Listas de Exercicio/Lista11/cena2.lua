818704c3727b2ee98660f8ecc36ad95621d3d8cd
Melqui <melki2011@gmail.com>
2019-05-31 19:25:17 -0300

    Lista

--- Listas de Exercicio/Lista11/cena2.lua
+++ Listas de Exercicio/Lista11/cena2.lua
@@ -52,45 +52,48 @@ local function mudabg()
     cont = cont+1
 end
 
-local button = display.newRect(w *.5, h *.0, 100, 30)
+local button = display.newRect(sceneGroup, w *.5, h *.0, 100, 30)
 button:setFillColor(0, 1, 0)
 button:addEventListener("tap", mudabg)
 local function updateRelogio()
     local date = os.date("*t")
     if cont == 0 then
-        bg = display.newImage("r3.jpg", w  * .5, w * .77)
+        bg = display.newImage(sceneGroup, "r3.jpg", w  * .5, w * .77)
         bg:scale( 0.9, 0.9 )
-        local h = display.newText(date.hour, w*.14,w*.75, native.systemFont, 30)
-        local m = display.newText(date.min, w*.51,w*.75, native.systemFont, 30)
-        local s = display.newText(date.sec, w*.87,w*.75, native.systemFont, 30)
+        local h = display.newText(sceneGroup, date.hour, w*.14,w*.75, native.systemFont, 30)
+        local m = display.newText(sceneGroup, date.min, w*.51,w*.75, native.systemFont, 30)
+        local s = display.newText(sceneGroup, date.sec, w*.87,w*.75, native.systemFont, 30)
         
         h:setFillColor(0, 0.5, 1)
         m:setFillColor(0.4, 1, 0.9)
         s:setFillColor(0, 0, 0)
     elseif cont == 1 then
-        bg = display.newImage("r4.jpg", w  * .5, w * .77)
+        bg = display.newImage(sceneGroup, "r4.jpg", w  * .5, w * .77)
         bg:scale( 0.6, 0.7 )
-        local h = display.newText(date.hour, w*.13,w*.75, native.systemFont,80)
-        local m = display.newText(date.min, w*.65,w*.76, native.systemFont, 28)
-        local s = display.newText(date.sec, w*.90,w*.76, native.systemFont, 30)
+        local h = display.newText(sceneGroup, date.hour, w*.13,w*.75, native.systemFont,80)
+        local m = display.newText(sceneGroup, date.min, w*.65,w*.76, native.systemFont, 28)
+        local s = display.newText(sceneGroup, date.sec, w*.90,w*.76, native.systemFont, 30)
         
         h:setFillColor(0, 0.5, 1)
         m:setFillColor(0.4, 0.1, 0.9)
         s:setFillColor(0, 0, 0)
     elseif cont == 2 then
-        bg = display.newImage("r5.jpg", w  * .5, w * .77)
+        bg = display.newImage(sceneGroup, "r5.jpg", w  * .5, w * .77)
         bg:scale( 0.6, 0.8 )
-        local h = display.newText(date.hour, w*.10,w*.75, native.systemFont, 30)
-        local m = display.newText(date.min, w*.51,w*.75, native.systemFont, 30)
-        local s = display.newText(date.sec, w*.85,w*.75, native.systemFont, 30)
+        local h = display.newText(sceneGroup, date.hour, w*.10,w*.75, native.systemFont, 30)
+        local m = display.newText(sceneGroup, date.min, w*.51,w*.75, native.systemFont, 30)
+        local s = display.newText(sceneGroup, date.sec, w*.85,w*.75, native.systemFont, 30)
         
         h:setFillColor(0, 0.5, 1)
         m:setFillColor(0.4, 0.1, 0.9)
         s:setFillColor(1, 1, 1)
     end
-    --
+    local button = display.newRect(sceneGroup, w *.5, h *.9, 100, 30)
+    button:setFillColor(0, 1, 0)
+    button:addEventListener("tap", mudacena)
+    sceneGroup:insert(button)
 end
-
+    
 updateRelogio()
 timer.performWithDelay(1000, updateRelogio, 0)
     
